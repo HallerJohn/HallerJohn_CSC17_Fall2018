fc7f295208c4a05c807f69792f38246d189a71aa
john haller <4jfhaller@gmail.com>
2018-09-17 14:03:39 -0700

    Signed-off-by: john haller <4jfhaller@gmail.com>

--- Class/ClassWork-9-12-18/Adding_Binary/main.cpp
+++ Class/ClassWork-9-12-18/Adding_Binary/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 //Global Constants - Math/Physics Constants, Conversions,
 //                   2-D Array Dimensions
-int length(string &,string &);
+void length(string &,string &);
 
 //Function Prototypes
 
@@ -23,42 +23,32 @@ int main(int argc, char** argv) {
     //Declare Variables
     string num1;
     string num2;
-    string answer;
-    int size=length(num1,num2);
-    int carry=0;
     
     //Initialize Variables
     cout<<"input 2 binary numbers"<<endl;
     cin>>num1>>num2;
     
+    //Make them equal length
+    length(num1,num2);
+    cout<<"Number 1 = "<<num1<<endl<<"Number 2 = "<<num2<<endl;
     //Process/Map inputs to outputs
-    for(int i=0;i<size+1;i++){
-        int digit1= num1.at(i)-'0';
-        int digit2= num2.at(i)-'0';
-        int sum = (digit1 ^ digit2 ^ carry)+'0';
-        answer= (char)sum+answer;
-        carry=(digit1 & digit2) | (digit2 & carry) | (digit1 & carry);
-    }
-    if(carry){
-        answer+='1';
-    }
+    
     //Output data
-    cout<<"The answer is ="<<answer<<endl;
+    
     //Exit stage right!
     return 0;
 }
 
-int length( string &num1, string &num2){
+void length( string &num1, string &num2){
     int length1=num1.size();
     int length2=num2.size();
     if(length1>length2){
         for(int i=0;i<length1-length2;i++){
-            num1='0'+num1;
-        }
-        return length2;
-    }else if(length2>length1){
-        for(int i=0;i<length2-length1;i++){
             num2='0'+num2;
         }
-    }return length1;
+    }else if(length2>length1){
+        for(int i=0;i<length2-length1;i++){
+            num1='0'+num1;
+        }
+    }
 }
---
+++ Class/ClassWork-9-17-18/Structures_Of_1DAnd2DArrays/Structure_of_1D_Dynamic_Array/Struc1DArray.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: John Haller
+ * Created on September 5th, 2018, 1:20 PM
+ * Purpose:  Create a CSC/CIS 5 Template
+ */
+
+
+
+#ifndef STRUC1DARRAY_H
+#define STRUC1DARRAY_H
+
+struct DynAry1{
+    int size;
+    int *array;
+};
+
+
+#endif /* STRUC1DARRAY_H */
+
