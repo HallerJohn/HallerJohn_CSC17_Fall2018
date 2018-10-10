3b4c96cfe857a5de78b7cea273da1c2229b2eda2
john haller <4jfhaller@gmail.com>
2018-10-10 13:58:56 -0700

    Signed-off-by: john haller <4jfhaller@gmail.com>

---
+++ Class/ClassWork-10-10-18/Class_of_1D_Dynamic_Array_1/Class1DArray.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: John Haller
+ * Created on September 5th, 2018, 1:20 PM
+ * Purpose:  Create a CSC/CIS 5 Template
+ */
+
+
+
+#ifndef CLASS1DARRAY_H
+#define CLASS1DARRAY_H
+
+class DynAry1{
+    private:
+        int size;
+        int *array;
+public:
+};
+
+
+#endif /* CLASS1DARRAY_H */
+
